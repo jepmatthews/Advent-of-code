8b1bfdf4fa17f0f5a8e8fc14286d0dd7017d5358
James Matthews <james.ep.matthews@gmail.com>
2024-12-02 23:46:25 +0000

    Tidy Day 2 Part 1

--- 2024/Day02/Part1/main.py
+++ 2024/Day02/Part1/main.py
@@ -1,12 +1,6 @@
-def report_is_safe(line):
-    prevDiff = 0
-    report = line.split()
-    for (index, n) in list(enumerate(report))[1:]:
-        diff = int(n) - int(report[index-1])
-        if(diff * prevDiff < 0 or abs(diff) > 3 or diff == 0):
-            return False
-        prevDiff = diff
-    return True
+def report_is_safe(report):
+    diffs = list(map(lambda a,b: int(b) - int(a), report[0:-1],report[1:]))
+    return all(map(lambda d: 0 < d <= 3, diffs)) or all(map(lambda d: 0 < -d <= 3, diffs))
 
 f = open("../input.txt")
-print(list((map(report_is_safe,f.readlines()))).count(True))
+print(list((map(lambda lin: report_is_safe(lin.split()),f.readlines()))).count(True))
