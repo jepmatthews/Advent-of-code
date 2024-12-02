0be6edc05c2afb21298bc910b57d9ccf9ff7541f
James Matthews <james.ep.matthews@gmail.com>
2024-12-02 21:57:39 +0000

    Day 1 part 2

---
+++ 2024/Day01/Part2/main.py
@@ -0,0 +1,17 @@
+f = open("../input.txt")
+dictionary = dict()
+for str in f.read().splitlines():
+    split = str.split()
+    x = int(split[0])
+    if x in dictionary:
+        dictionary[x] = (dictionary[x][0] + 1, dictionary[x][1])
+    else:
+        dictionary[x] = (1,0)
+    y = int(split[1])
+    if y in dictionary:
+        dictionary[y] = (dictionary[y][0] , dictionary[y][1] + 1)
+    else:
+        dictionary[y] = (0,1)
+print (dictionary)
+result = sum(map(lambda a: a[0]*a[1][0]*a[1][1],dictionary.items()))
+print(result)
