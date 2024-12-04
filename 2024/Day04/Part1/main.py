0d18e85cac481ac96fe4c08fddb4197278b68839
James Matthews <j.matthews@codat.io>
2024-12-04 14:25:14 +0000

    Day 4 updates

--- 2024/Day04/Part1/main.py
+++ 2024/Day04/Part1/main.py
@@ -17,9 +17,10 @@ class DiagonalIterator:
             elif self.direction == "SE":
                 self.x += 1
                 self.y += 1
+
             return result
 
-def countXmas(string):
+def count_xmas(string):
     return string.count("XMAS") + string.count("SAMX")
 
 f = open("../input.txt")
@@ -27,11 +28,11 @@ count = 0
 lines = f.readlines()
 lines = list(map(lambda str: str.replace("\n",""), lines))
 for line in lines:
-    count += countXmas(line)
+    count += count_xmas(line)
 
 x,y = 0,0 
 while(x < len(lines[0])):
-    count += countXmas(''.join(DiagonalIterator(lines,x,y,'NE')))
+    count += count_xmas(''.join(DiagonalIterator(lines,x,y,'NE')))
     if y < len(lines) - 1:
         y += 1
     else:
@@ -39,7 +40,7 @@ while(x < len(lines[0])):
 
 x,y = 0,len(lines) - 1
 while(x < len(lines[0])):
-    count += countXmas(''.join(DiagonalIterator(lines,x,y,'SE')))
+    count += count_xmas(''.join(DiagonalIterator(lines,x,y,'SE')))
     if y > 0:
         y -= 1
     else:
@@ -48,6 +49,6 @@ while(x < len(lines[0])):
 lines = list(map(list,zip(*lines)))
 
 for line in lines:
-    count += countXmas(''.join(line))
+    count += count_xmas(''.join(line))
 
 print(count)
--- 2024/Day04/Part2/main.py
+++ 2024/Day04/Part2/main.py
@@ -1,59 +1,12 @@
-class DiagonalIterator:
-    def __init__(self, array,startX,startY,direction):
-        self.array = array
-        self.x = startX
-        self.y = startY
-        self.direction = direction
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if(self.y >= len(self.array) or self.x >= len(self.array[0]) or self.y < 0 or self.x < 0):
-            raise StopIteration
-        else:
-            result = self.array[self.y][self.x]
-            if self.direction == "NE":
-                self.x += 1
-                self.y -= 1
-            elif self.direction == "SE":
-                self.x += 1
-                self.y += 1
-            return result
-
-def countXmas(string):
-    return string.count("XMAS") + string.count("SAMX")
-
 f = open("../input.txt")
-count = 0
+result = 0
 lines = f.readlines()
-lines = list(map(lambda str: str.replace("\n",""), lines))
-for line in lines:
-    count += countXmas(line)
 
-x = 0
-y = 0
-while(True):
-    if x >= len(lines[0]):
-        break
-    count += countXmas(''.join(iter(DiagonalIterator(lines,x,y,'NE'))))
-    if y < len(lines) - 1:
-        y += 1
-    else:
-        x += 1
+for y in range(1,len(lines) - 1):
+    for x in range(1,len(lines[0]) - 1):
+        if lines[y][x] == "A":
+            neighbours = [lines[y+1][x+1],lines[y+1][x-1],lines[y-1][x+1],lines[y-1][x-1]]
+            if(neighbours.count("M") == 2 and neighbours.count("S") == 2 and lines[y+1][x+1] != lines[y-1][x-1]):
+                result += 1
 
-x = 0
-y = len(lines) - 1
-while(True):
-    if x >= len(lines[0]):
-        break
-    count += countXmas(''.join(iter(DiagonalIterator(lines,x,y,'SE'))))
-    if y > 0:
-        y -= 1
-    else:
-        x += 1
-
-lines = list(map(list,zip(*lines)))
-
-for line in lines:
-    count += countXmas(''.join(line))
-
-print(count)
+print(result)
