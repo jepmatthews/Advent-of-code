ae7771be5966c8ec4a7b6b1401f2d6651c2a3058
James Matthews <james.ep.matthews@gmail.com>
2025-12-11 21:45:32 +0000

    Missed some bits

--- 2025/Day02/Part1/main.go
+++ 2025/Day02/Part1/main.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	"github.com/golang-design/clipboard"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	result := "0"
 	f, err := os.Open("../input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -18,16 +17,43 @@ func main() {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+	result := 0
+	text := scanner.Text()
 
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+	for _, str := range strings.Split(text, ",") {
+		split := strings.Split(str, "-")
+		fmt.Printf("Start: %s, End: %s\n", split[0], split[1])
+		rangeStart, err := strconv.Atoi(split[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		rangeEnd, err := strconv.Atoi(split[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for val := rangeStart; val <= rangeEnd; val++ {
+			if isPalindrome(val) {
+				fmt.Printf("%d\n", val)
+				result += val
+			}
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	err := clipboard.Init()
-	if err != nil {
-		log.Fatal(err)
+	fmt.Printf("result: %d", result)
+}
+
+func isPalindrome(x int) bool {
+	str := strconv.Itoa(x)
+	if len(str)%2 != 0 {
+		return false
 	}
+	if str[0:len(str)/2] == str[len(str)/2:] {
+		return true
+	}
+	return false
 }
